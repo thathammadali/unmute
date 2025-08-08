01ed5b7872aee8b923f092948f779b0ffe2da754
Hammad Ali <thathammadali@gmail.com>
2025-08-08 21:39:22 +0500

    - Added "Copy to Clipboard" for RoomID

---
+++ src/app/room/[id]/TopBar.tsx
@@ -0,0 +1,23 @@
+import { ActionButton } from '@/app/room/[id]/bottomBar/Buttons';
+import { BiClipboard } from 'react-icons/bi';
+
+export default function TopBar({ roomId }: { roomId: string }) {
+    const copyToClipBoard = async () => {
+        await navigator.clipboard.writeText(roomId);
+        alert('Room ID copied to clipboard!');
+    };
+
+    return (
+        <div
+            className={
+                'flex h-20 w-full items-center justify-center gap-2 bg-neutral-600 text-white'
+            }
+        >
+            <h2>{roomId}</h2>
+            <ActionButton
+                icon={<BiClipboard />}
+                onClickHandler={copyToClipBoard}
+            />
+        </div>
+    );
+}
